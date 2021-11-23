b4e8bd8857281b63c3db29657a8272cc1b041537
Carlos <carlos.r.goularte@hotmail.com>
2021-11-23 20:32:13 -0300

    Primeiros estudos sobre POO

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Estudos_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
