d6c4b2b61a82b68956699afba05024752bcf64e7
Carlos <carlos.r.goularte@hotmail.com>
2021-11-24 20:08:46 -0300

    Segundo Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,32 @@ namespace Estudos_POO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoas p1 = new Pessoas();
+            Pessoas p2 = new Pessoas();
+
+            Console.Write("Digite seu nome: ");
+            p1.Nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            p1.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome de seu companheir@: ");
+            p2.Nome = Console.ReadLine();
+
+            Console.Write("Digite a idade de seu companheir@: ");
+            p2.Idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da Pessoa1: " + "Nome: " + p1.Nome + "  Idade: " + p1.Idade);
+            Console.WriteLine("Dados da Pessoa2: " + "Nome: " + p2.Nome + "  Idade: " + p2.Idade);
+
+            if (p1.Idade < p2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + p2.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + p1.Nome);
+            }
         }
     }
 }
