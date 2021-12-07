53c989bd27d49eaa7eb46ffc84436f6c5acac6d4
Carlos <carlos.r.goularte@hotmail.com>
2021-12-07 20:45:35 -0300

    Fix repo

---
+++ exercício 1/Exercício 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercício_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
