a46a74bc73f8f60128c875c6333efb302c880f95
Carlos <carlos.r.goularte@hotmail.com>
2021-12-07 21:56:43 -0300

    Conclusão exercício 2

---
+++ Exercício2/Funcionario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Exercício2
+{
+    class Funcionario
+    {
+
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + SalarioBruto * (porcentagem / 100);
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", R$"
+                + SalarioLiquido().ToString("F2");
+        }
+
+
+    }
+}
---
+++ Exercício2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercício2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionario = new Funcionario();
+
+            Console.WriteLine("Digite os dados do funcionário:");
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(funcionario);
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+            funcionario.AumentarSalario(porcentagem);
+
+            Console.WriteLine(funcionario);
+    
+
+        }
+    }
+}
