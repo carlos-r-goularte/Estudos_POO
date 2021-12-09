8306558934fd4f041fdf3c1a54b71b96fce3e7bc
Carlos <carlos.r.goularte@hotmail.com>
2021-12-08 21:45:50 -0300

    Testes Membros Estaticos

---
+++ TestesMembrosEstaticos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TestesMembrosEstaticos
+{
+    class Program
+    {
+        static double Pi = 3.14;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+
+            double circunferencia = Circunferencia(raio);
+            double volume = Volume(raio);
+            Console.WriteLine("Circunferência: " + circunferencia.ToString("F2"));
+
+            Console.WriteLine("Volume: " + volume.ToString("F2"));
+            Console.WriteLine("Valor de Pi: " + Pi);
+        }
+
+        static double Circunferencia(double r)
+        {
+            return 2 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
