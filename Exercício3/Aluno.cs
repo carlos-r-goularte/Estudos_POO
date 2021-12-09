b780b5737b0ef966c2d9d0193013f4581ee24d31
Carlos <carlos.r.goularte@hotmail.com>
2021-12-08 21:25:56 -0300

    Exercicio 3 concluido

---
+++ Exercício3/Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercício3
+{
+    class Aluno
+    {
+        public string Nome;
+        public double Nota1, Nota2, Nota3;
+
+        public bool Aprovacao()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+         
+        }
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3;
+        }
+        public double Restante()
+        {
+            return 100 - (Nota1 + Nota2 + Nota3);
+        }
+    }
+}
---
+++ Exercício3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exercício3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota1 = double.Parse(Console.ReadLine());
+            aluno.Nota2 = double.Parse(Console.ReadLine());
+            aluno.Nota3 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("NOTA FINAL: "+ aluno.NotaFinal().ToString("F2"));
+
+            if (aluno.Aprovacao())
+            {
+                Console.WriteLine("Aluno Aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Aluno Reprovado!");
+                Console.WriteLine("Faltaram: "+ aluno.Restante().ToString("F2") + " pontos!" );
+            }
+        }
+    }
+}
