622f3d29de496f6167a4aa574c062245700c3dfd
Carlos <carlos.r.goularte@hotmail.com>
2021-12-13 21:03:43 -0300

    Criação e chamadas de metodos estáticos

--- TestesMembrosEstaticos/Program.cs
+++ TestesMembrosEstaticos/Program.cs
@@ -4,29 +4,21 @@ namespace TestesMembrosEstaticos
 {
     class Program
     {
-        static double Pi = 3.14;
 
         static void Main(string[] args)
         {
+
             Console.WriteLine("Valor do raio: ");
             double raio = double.Parse(Console.ReadLine());
 
-            double circunferencia = Circunferencia(raio);
-            double volume = Volume(raio);
+            /* Não há necessidade de instanciar objetos da classe, sendo que os metodos são estáticos*/
+            double circunferencia = Calculadora.Circunferencia(raio);    
+            double volume = Calculadora.Volume(raio);
+
             Console.WriteLine("Circunferência: " + circunferencia.ToString("F2"));
 
             Console.WriteLine("Volume: " + volume.ToString("F2"));
-            Console.WriteLine("Valor de Pi: " + Pi);
-        }
-
-        static double Circunferencia(double r)
-        {
-            return 2 * Pi * r;
-        }
-
-        static double Volume(double r)
-        {
-            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi);
         }
     }
 }
