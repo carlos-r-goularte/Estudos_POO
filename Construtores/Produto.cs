5f08ef5458509e531befed54d61bc03b4c460831
Carlos <carlos.r.goularte@hotmail.com>
2021-12-14 21:52:04 -0300

    Aula Construtores

--- Construtores/Produto.cs
+++ Construtores/Produto.cs
@@ -8,5 +8,39 @@ namespace Construtores
 {
     class Produto
     {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2")
+                + ", "
+                + Quantidade
+                + " unidade, Total: $"
+                + ValorTotalEmEstoque().ToString("F2");
+        }
     }
 }
--- Construtores/Program.cs
+++ Construtores/Program.cs
@@ -6,7 +6,84 @@ namespace Construtores
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.Write("Digite o nome do produto: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite o valor do produto: ");
+            double preco = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantidade em estoque do produto: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto produto = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Produto cadastrado com sucesso!");
+            Console.WriteLine();
+
+            string opcao = Menu();
+
+            while (opcao.ToUpper() != "X")
+            {
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine();
+                        Console.WriteLine("==================");
+                        Console.WriteLine("Você irá adicionar produtos ao seu estoque!");
+                        Console.WriteLine("==================");
+                        Console.Write("Digite a quantidade de produtos que serão adicionados: ");
+                        int qte = int.Parse(Console.ReadLine());
+                        produto.AdicionarProdutos(qte);
+                        break;
+
+                    case "2":
+                        Console.WriteLine();
+                        Console.WriteLine("==================");
+                        Console.WriteLine("Você irá remover produtos ao seu estoque!");
+                        Console.WriteLine("==================");
+                        Console.Write("Digite a quantidade de produtos que serão adicionados: ");
+                        qte = int.Parse(Console.ReadLine());
+                        produto.RemoverProdutos(qte);
+                        break;
+
+                    case "3":
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Dados do produto: " + produto);
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+
+                    default:
+                        break;
+                }
+
+                opcao = Menu();
+            }
+
+            Console.WriteLine("Até mais!");
+        }
+
+        private static string Menu()
+        {
+
+            Console.WriteLine("==================");
+            Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("==================");
+
+            Console.WriteLine("1 - Adicionar Produtos ao Estoque");
+            Console.WriteLine("2 - Remover Produtos do Estoque");
+            Console.WriteLine("3 - Visualizar Estoque");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcao = Console.ReadLine().ToUpper();
+
+            Console.WriteLine();
+
+            return opcao;
         }
     }
 }
